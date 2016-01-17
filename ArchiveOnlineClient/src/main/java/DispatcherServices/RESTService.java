8bb4f3342a349fc57831f72e91fde5f979b669f5
ToshaBritva <mineloveguitar@gmail.com>
2016-01-17 22:26:23 +0500

    Добавил проект клиента

---
+++ ArchiveOnlineClient/src/main/java/DispatcherServices/RESTService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DispatcherServices;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import java.util.HashMap;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.UriBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author minel
+ */
+public class RESTService {
+
+    protected Client client;
+    protected WebResource service;
+
+    public RESTService(String serviceUrl) {
+        client = Client.create();
+        service = client.resource(UriBuilder.fromUri(serviceUrl).build());
+    }
+
+    //Возвращает Массив JSONов от сервиса
+    public JSONArray getResponse(String resourcePath, MultivaluedHashMap<String, String> params) throws ParseException {
+
+        //Получаем ответ сервера в виде строки
+        String serviceResponse = service.path(resourcePath).queryParams(params).accept(MediaType.APPLICATION_JSON).get(String.class);
+
+        //Парсим из ответа JSON
+        JSONParser parser = new JSONParser();
+
+        //И возвращаем его
+        return (JSONArray) parser.parse(serviceResponse);
+    }
+
+}
